69553e24c96780e5cd29472d8d5528ad5ea07789
Benjamin Diele <benjamin.diele@cegeka.com>
2017-10-11 14:26:07 +0200

    Routing Ex5 completed

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,13 @@
+import { AboutComponent } from './about.component';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+    imports: [
+        RouterModule.forChild([
+            { path: '', component: AboutComponent }
+        ])
+    ],
+    declarations: [AboutComponent]
+})
+export class AboutModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -30,6 +30,6 @@ export const APP_ROUTES: Routes =
                 }
             ]
         },
-        { path: 'about', component: AboutComponent },
+        { path: 'about', loadChildren: './about/about.module#AboutModule' },
         { path: '**', redirectTo: 'list' }
     ]
